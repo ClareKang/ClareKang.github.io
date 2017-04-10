8ba59206094f6831faacd0ec43e17ee76e960f46
한영준 <reverof81@gmail.com>
2017-04-10 22:17:21 +0900

    jpa repository interface 변경

--- src/main/java/net/meshkorea/mcp/api/repository/PricingPlanRepository.java
+++ src/main/java/net/meshkorea/mcp/api/repository/PricingPlanRepository.java
@@ -2,11 +2,11 @@ package net.meshkorea.mcp.api.repository;
 
 import net.meshkorea.mcp.api.config.data.VroongDbConfig;
 import net.meshkorea.mcp.api.entity.pricing.PricingPlan;
-import net.meshkorea.platform.core.web.repository.GenericRepository;
+import net.meshkorea.platform.core.web.repository.JpaSpecificationRepository;
 
 /**
  * Created by yjhan on 2017. 2. 3..
  */
 @VroongDbConfig.VroongData
-public interface PricingPlanRepository extends GenericRepository<PricingPlan, Long> {
+public interface PricingPlanRepository extends JpaSpecificationRepository<PricingPlan, Long> {
 }
