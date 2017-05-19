adfc1bb9eeb8a16ec9c023e9b00b3bdcb84929d7
한영준 <reverof81@gmail.com>
2017-05-19 17:45:34 +0900

    주소 검색 부분 삭제

--- src/main/java/net/meshkorea/mcp/api/service/common/IntraCommonService.java
+++ src/main/java/net/meshkorea/mcp/api/service/common/IntraCommonService.java
@@ -1,8 +1,6 @@
 package net.meshkorea.mcp.api.service.common;
 
 import com.meshprime.api.client.model.Bank;
-import com.meshprime.api.client.model.Regions;
-import com.meshprime.intra.api.IntraRegionsApi;
 import com.meshprime.intra.api.IntraStoresApi;
 import com.meshprime.intra.service.auth.IntraTokenService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,23 +20,8 @@ public class IntraCommonService {
     @Autowired
     IntraStoresApi intraStoresApi;
 
-    @Autowired
-    IntraRegionsApi intraRegionsApi;
-
     public List<Bank> getBanks() throws Exception {
         return intraStoresApi.listBanks(intraTokenService.getAuthToken());
-    }
-
-    public List<Regions> listSiDo() throws Exception {
-        return intraRegionsApi.listRegionsSiDo(intraTokenService.getAuthToken());
-    }
-
-    public List<Regions> listSiGunGU(Integer parentCode, String siDo) throws Exception {
-        return intraRegionsApi.listSiGunGuByCode(intraTokenService.getAuthToken(), parentCode, siDo);
-    }
-
-    public List<Regions> listEupMyeonDongRi(Integer parentCode, String siDo, String siGunGu, Boolean onlyAdminDong, Boolean onlyLegalDong) throws Exception {
-        return intraRegionsApi.listEupMyeonDongByCode(intraTokenService.getAuthToken(), parentCode, siDo, siGunGu, onlyAdminDong, onlyLegalDong);
     }
 
 }
