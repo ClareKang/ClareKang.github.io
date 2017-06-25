530479225bf981931b47767e59b8365f6ac191e3
한영준 <yjhan@meshkorea.net>
2017-06-25 23:12:42 +0900

    health check url 추가

--- src/main/java/net/meshkorea/mcp/api/controller/common/McpCommonController.java
+++ src/main/java/net/meshkorea/mcp/api/controller/common/McpCommonController.java
@@ -3,6 +3,7 @@ package net.meshkorea.mcp.api.controller.common;
 import net.meshkorea.mcp.api.domain.model.auth.CodeListResponse;
 import net.meshkorea.mcp.api.service.common.McpCommonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,11 @@ public class McpCommonController {
 
     @Autowired
     private McpCommonService mcpCommonService;
+
+    @GetMapping("/health")
+    public ResponseEntity<?> health() {
+        return ResponseEntity.ok("OK");
+    }
 
     @GetMapping("/userTypes")
     public CodeListResponse getUserTypes() {
