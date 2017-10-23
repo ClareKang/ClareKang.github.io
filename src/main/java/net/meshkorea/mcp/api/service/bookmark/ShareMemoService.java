282e4552d7b38855e4f520ac9a1a4aae8852e94c
rsupjy <rsupjy@gmail.com>
2017-10-23 18:51:24 +0900

    MESHONE-55 Add bookmark update bulk share memo

--- src/main/java/net/meshkorea/mcp/api/service/bookmark/ShareMemoService.java
+++ src/main/java/net/meshkorea/mcp/api/service/bookmark/ShareMemoService.java
@@ -1,13 +1,18 @@
 package net.meshkorea.mcp.api.service.bookmark;
 
+import net.meshkorea.mcp.api.domain.entity.bookmark.Bookmark;
 import net.meshkorea.mcp.api.domain.entity.bookmark.ShareMemo;
+import net.meshkorea.mcp.api.domain.model.bookmark.BookmarkType;
 import net.meshkorea.mcp.api.domain.model.bookmark.ShareMemoRequest;
 import net.meshkorea.mcp.api.domain.model.bookmark.ShareMemoType;
+import net.meshkorea.mcp.api.domain.repository.BookmarkRepository;
 import net.meshkorea.mcp.api.domain.repository.ShareMemoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class ShareMemoService {
@@ -15,6 +20,10 @@ public class ShareMemoService {
     @Autowired
     private ShareMemoRepository shareMemoRepository;
 
+    @Autowired
+    private BookmarkRepository bookmarkRepository;
+
+    @Transactional
     public ShareMemo saveMemo(ShareMemoRequest.SaveMemo req) {
         // 이미 저장된 메모
         ShareMemo memo = shareMemoRepository.findByShareMemoTypeAndShareMemoId(ShareMemoType.valueOf(req.getShareMemoType()), req.getShareMemoId());
@@ -30,7 +39,15 @@ public class ShareMemoService {
         memo.setShareMemoId(req.getShareMemoId());
         memo.setShareMemoType(ShareMemoType.valueOf(req.getShareMemoType()));
 
-        return shareMemoRepository.save(memo);
+        ShareMemo saveMemo = shareMemoRepository.save(memo);
+
+        // 북마크 업데이트
+        bookmarkRepository.updateBulkShareMemoNo(
+                saveMemo,
+                BookmarkType.valueOf(req.getShareMemoType()),
+                req.getShareMemoId());
+
+        return saveMemo;
     }
 
     public ShareMemo findMemo(ShareMemoRequest.FindMemo req) {
